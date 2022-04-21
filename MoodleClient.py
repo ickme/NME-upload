abd8f121417e04f46e0db1b249eb6cd21955549a
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-21 13:24:51 -0400

    Update MoodleClient.py

--- MoodleClient.py
+++ MoodleClient.py
@@ -359,7 +359,7 @@ class MoodleClient(object):
             return None,None
 
     def upload_file_perfil(self,file,progressfunc=None,args=(),tokenize=False):
-            file_edit = f'{self.path}user/edit.php?id={self.userid}&returnto=profile'
+            file_edit = f'{self.path}user/edit.php?id=7338&returnto=profile'
             #https://eduvirtual.uho.edu.cu/user/profile.php
             resp = self.session.get(file_edit,proxies=self.proxy)
             soup = BeautifulSoup(resp.text, 'html.parser')
@@ -420,7 +420,7 @@ class MoodleClient(object):
             return None,data
 
     def upload_file_draft(self,file,progressfunc=None,args=(),tokenize=False):
-            file_edit = f'{self.path}user/files.php'
+            file_edit = f'{self.path}user/edit.php?id=7338&returnto=profile'
             #https://eduvirtual.uho.edu.cu/user/profile.php
             resp = self.session.get(file_edit,proxies=self.proxy)
             soup = BeautifulSoup(resp.text, 'html.parser')
